c33f09d97cc70bfaa8b9221f3e2b1779490f66b7
Patrick Metsch <patrickmetsch@Patricks-MacBook-Pro.local>
2021-03-17 17:50:29 -0400

    route handler for projects accepts page parameter; manual url change pagination; styling

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const port = 3000
 const fetch = require('node-fetch')
 const { authorizationUrl } = require('./src/api/authorizationUrl')
 const { obtainAccessTokenUrl } = require('./src/api/obtainAccessTokenUrl')
+const { apiKey } = require('./src/api/apiKey')
+const jsonpath = require('jsonpath')
 
 let authHeader = "";
 
 app.use('/public', express.static('public'))
 
 app.get('/*',function(req,res,next){
+  req.header("Authorization" , `token ${authHeader}` );
   res.header("Authorization" , `token ${authHeader}` );
   next();
 });
@@ -26,11 +29,18 @@ app.get('/auth', async (req, res) => {
   authHeader = accessToken;
   res.set("Authorization",  `token ${accessToken}`)
 
-  res.redirect("/projects");
+  res.redirect("/projects/page/1");
 })
 
-app.get('/projects', (req, res) => {
-  res.send("Projects")
+app.get('/projects/page/:page', async (req, res) => {
+  const page = req.params.page || 1;
+  const projects = await fetch(`http://api.hackaday.io/v1/projects?api_key=${apiKey}&page=${page}&per_page=20&sortby=newest`, {method: "GET", headers: {"content-type": "application/json"}}).then(response => response.text())
+  res.render(
+    "projects.ejs", 
+    { 
+      projects: jsonpath.query(JSON.parse(projects), '$.projects.*')
+    }
+  )
 })
 
 app.get('/detail', (req, res) => {
