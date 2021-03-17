6ad43f5c228f89619d9dda7c404bf391638883f2
Patrick Metsch <patrickmetsch@Patricks-MacBook-Pro.local>
2021-03-17 16:31:26 -0400

    complete auth process

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ const fetch = require('node-fetch')
 const { authorizationUrl } = require('./src/api/authorizationUrl')
 const { obtainAccessTokenUrl } = require('./src/api/obtainAccessTokenUrl')
 
+let authHeader = "";
+
 app.use('/public', express.static('public'))
 
+app.get('/*',function(req,res,next){
+  res.header("Authorization" , `token ${authHeader}` );
+  next();
+});
+
 app.get('/', async (req, res) => {
-  // const text = await fetch(authorizationUrl, { method: "GET" }).then(response => response.text())
-  res.render("home.ejs")
+  res.redirect(authorizationUrl);
 })
 
-app.get('/auth/:code', async (req, res) => {
+app.get('/auth', async (req, res) => {
   const code = req.query.code;
-  // const text = await fetch(obtainAccessTokenUrl(code), {method: "GET"}).then(response => response.text())
-  res.send(text)
+  const text = await fetch(obtainAccessTokenUrl(code), {method: "GET"}).then(response => response.text())
+  const accessToken = JSON.parse(text).access_token;
+
+  authHeader = accessToken;
+  res.set("Authorization",  `token ${accessToken}`)
+
+  res.redirect("/projects");
 })
 
 app.get('/projects', (req, res) => {
